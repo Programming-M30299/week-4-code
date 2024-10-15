782d47bc4ba497a17055fd724c9d0c896f87f061
manighahrmani <mashapicasso@gmail.com>
2024-10-15 16:24:34 +0100

    Update casing and naming of files

--- pract4.py
+++ pract4.py
@@ -1,14 +1,14 @@
 import os
 
 
-def studentInfo():
+def student_info():
     course = input("What's your course? ")
-    studentID = input("What's your ID number? ")
+    student_id = input("What's your ID number? ")
     print("Welcome to:\t" + course)
-    print("\n" * 2 + "Your ID number is:\t" + studentID[2:])
+    print("\n" * 2 + "Your ID number is:\t" + student_id[2:])
 
 
-def personalDetails():
+def personal_details():
     name = input("What's your name? ")
     age = int(input("What's your age? "))
     height = float(input("What's your height? "))
@@ -16,46 +16,33 @@ def personalDetails():
     print(f"name:\t{name:>10}\nage:\t{age:>10}\nheight:\t{height:>10.2f}")
 
 
-def readQuote():
+def read_quote():
     print("Current directory:\t" + os.getcwd())
     print("Files in current directory:\t" + str(os.listdir()))
-
     # Change directory to the folder containing quotation.txt
-    os.chdir("textFiles")
+    os.chdir("text_files")
     # Checking if quotation.txt is in the current directory:
     print("Current directory:\t" + os.getcwd())
     print("Files in current directory:\t" + str(os.listdir()))
 
-    inFile = open("quotation.txt", "r")
+    input_file = open("quotation.txt", "r")
 
     # You can use `read()` to read the whole file into a single string
-    content = inFile.read()
+    content = input_file.read()
     print(content)
 
-    # Or use `readLines()` to read all lines into a list
-    # lines = inFile.readlines()
-    # for line in lines:
-    #     print(line[:-1])
 
-    # Or use `readLine()` to read one line at a time
-    # line = inFile.readline()
-    # while line:
-    #     print(line)
-    #     line = inFile.readline()
-
-    inFile.close()
-
-
-def writeQuote():
-    os.chdir("textFiles")
-    outFile = open("myQuotation.txt", "w")
+def write_quote():
+    os.chdir("text_files")
+    output_file = open("my_quotation.txt", "w")
 
     # You can use `write()` to write a single string
-    print("I love Python!", file=outFile)
-    print("(Matthew Poole)", file=outFile)
+    print("I love Python!", file=output_file)
+    print("(Matthew Poole)", file=output_file)
 
     # Or write both lines in one go separated by a newline character ('\n')
     # content = "I love Python!\n(Matthew Poole)"
-    # outFile.write(content)
+    # output_file.write(content)
 
-    outFile.close()
+
+# Solutions below:
